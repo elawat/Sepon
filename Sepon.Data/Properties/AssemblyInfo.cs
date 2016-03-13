b39f5948c65a5b977093a49d8f17d2cea80ba0d9
Ela <elawatroba@gmail.com>
2016-03-13 14:02:51 +0000

    Entity Model2

--- Sepon.Data/Properties/AssemblyInfo.cs
+++ Sepon.Data/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("12569284-8a10-4c9c-b4f9-606047858fdc")]
+[assembly: Guid("abc009fa-0306-44fe-a746-e449bd1f8619")]
 
 // Version information for an assembly consists of the following four values:
 //
