7096172c77318f9b9bb3e9f645027b79558ef49e
elawa <elawa@LAPTOP-P42C8U26>
2017-04-29 14:29:44 +0100

    Master page ParentData added

---
+++ master/ParentData.master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Sepon;
+
+public partial class master_ParentData : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        
+
+    }
+}
